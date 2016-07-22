c43a65c67ccd2918f19a32a77a41bb583a925844
Amo Chen <spitfire.sidra@gmail.com>
2016-07-23 00:02:16 +0800

    added brew.sh to make installation easier

---
+++ brew.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+
+brew update
+
+brew upgrade --all
+
+# install parallel, pee, ts, sponge ...
+brew install moreutils
+
+# install `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed
+brew install findutils
+
+# install `readelf`, `objdump`, ...
+brew install binutils
+
+# install gnu `sed`, overwriting the built-in `sed`
+brew install gnu-sed --with-default-names
+
+brew install bash bash-completion2
+
+brew install zsh zsh-completions
+
+brew install wget --with-iri
+
+brew install vim --override-system-vi
+
+# install other useful binaries
+brew install ack
+brew install git
+brew install ssh-copy-id
+brew install testssl
+brew install tree
+brew install p7zip
+brew install pigz
+brew install pv
+brew install rename
+
+# visual binary diff
+brew install vbindiff
+
+brew install imagemagick --with-webp
+
+# enhance quick look
+brew cask install qlcolorcode
+brew cask install qlstephen
+brew cask install qlmarkdown
+brew cask install quicklook-json
+brew cask install qlprettypatch
+brew cask install quicklook-csv
+brew cask install betterzipql
+brew cask install webpquicklook
+brew cask install suspicious-package
+
+brew cleanup
+
