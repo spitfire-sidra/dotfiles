774640ca290d896cb01996d1446f5d5b50f07ca9
Amo Chen <chimerhapsody@gmail.com>
2024-09-28 20:32:56 +0800

    cleanup homebrew.sh

--- macOS/homebrew.sh
+++ macOS/homebrew.sh
@@ -17,13 +17,15 @@ brew install binutils
 
 # install gnu `sed`, overwriting the built-in `sed`
 brew install gnu-sed
-brew install bash
 brew install bat
-brew install bash-completion@2
 brew install zsh zsh-completions
 brew install wget
 brew install vim
 brew install vifm
+
+# bash
+brew install bash
+brew install bash-completion@2
 
 # neovim
 brew install neovim
@@ -36,7 +38,12 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 brew install ack
 # ag
 brew install the_silver_searcher
+# fd-find
+brew install fd
+# rg
+brew install ripgrep
 #brew install git
+brew install lazygit
 #brew install ssh-copy-id
 brew install testssl
 brew install tree
@@ -50,7 +57,6 @@ brew install telnet
 brew install ctags
 brew install fzf
 brew install autojump
-brew install fd
 brew install jq
 brew install tmux
 brew install direnv
@@ -61,16 +67,9 @@ brew install vbindiff
 
 brew install imagemagick
 
-brew install ripgrep
-
 # nodejs
 brew install nvm
 brew install yarn
-
-# enhance quick look
-brew install --cask qlmarkdown
-brew install --cask quicklook-json
-brew install --cask quicklook-csv
 
 # python
 brew install pyenv
